53f8e189c36b724c83309b3d9803e51608d32047
shien <sienrizumu@gmail.com>
2017-04-02 20:07:39 +0900

    fix test hostname method.

--- modules/hostname.py
+++ modules/hostname.py
@@ -5,3 +5,13 @@ import subprocess
 
 def test_hostname(hostname):
     output = subprocess.check_output(["ssh", hostname, "hostname"])    
+    hostname = hostname + "\n"
+    if output == hostname:
+        return True
+    return False
+
+if __name__ == "__main__":
+    if test_hostname("dmc-gate1"):
+        print("[Passed]")
+    else:
+        print("[Failed]")
