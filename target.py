b7f680489986cae0cdd05a773cec87227f084d4f
shien <sienrizumu@gmail.com>
2017-04-09 00:06:49 +0900

    change class name.

--- target.py
+++ target.py
@@ -5,19 +5,19 @@ import json
 
 SERVER_CHECKER_MARK = "serverchecker"
 
-class Host:
-    def __init__(self, host_file):
+class Target:
+    def __init__(self, target_file):
         self.connection_name = ""
         self.data = {}
-        self.read_host_data(host_file)
+        self.read_target_data(target_file)
 
-    def read_host_data(self, host_file):
+    def read_target_data(self, target_file):
         tmp_data = ""
         try:
-            with open(host_file, 'r') as fd:
+            with open(target_file, 'r') as fd:
                 tmp_data = json.load(fd)
         except:
-            print("open failed: " + host_file)
+            print("open failed: " + target_file)
             pass
         if SERVER_CHECKER_MARK not in tmp_data:
             exit(1)
@@ -34,7 +34,7 @@ class Host:
         return self.connection_name
 
 if __name__ == "__main__":
-    host = Host("test_file/hostname1.json")
-    print(host.get_test_contents("default_gateway"))
-    print(host.get_connection_name())
+    target = Target("test_file/targetname1.json")
+    print(target.get_test_contents("default_gateway"))
+    print(target.get_connection_name())
 
